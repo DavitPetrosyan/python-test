8c5448e2ec9b130180cfacdedeec2259bbac7090
vardushik <46680990+vardushik@users.noreply.github.com>
2020-02-12 20:42:08 +0100

    Update prime_test.py

--- prime_test.py
+++ prime_test.py
@@ -8,7 +8,7 @@ x = sys.argv[1]
 
 def primetest(x):
     """ 
-    this function check if the input (have to be int) is prime number or not
+    this function checks if the input (have to be int) is prime number or not
     
     execution: from termianl run 
     $ python prime_test.py input
