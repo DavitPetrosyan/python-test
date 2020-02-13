58de2befc5d9eae1f280efc7fcf3b603c4605cae
vardushik <46680990+vardushik@users.noreply.github.com>
2020-02-13 11:51:03 +0100

    Add files via upload
    better version of testing the prime numbers

--- prime_test.py
+++ prime_test.py
@@ -2,10 +2,6 @@ import sys
 
 x = sys.argv[1]
 
-
-
-
-
 def primetest(x):
     """ 
     this function checks if the input (have to be int) is prime number or not
@@ -13,21 +9,12 @@ def primetest(x):
     execution: from termianl run 
     $ python prime_test.py input
     """
-
-    count = 0
-    for i in range(1,x):
+    for i in range(2,x):
         if x%i==0:
-            count+=1
-
-
-    if count == 1:
-        p = 'a'
+            print(x,' is not a prime number')
+            break
     else:
-        p = 'not'
+        print(x, ' is a prime number')
 
-    return p 
-
-
-print('{} is {} prime number'.format(x, primetest(int(x))))
-
-
+if __name__ == "__main__":
+    primetest(int(x))
